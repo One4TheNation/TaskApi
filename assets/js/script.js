123643a326280d1e69d1690a5e7f5711666aaffc
Navarre <jpnavarre@Navarres-iMac.local>
2024-05-30 14:20:02 -0400

    fixing volation error

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,63 +5,85 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
     return Math.random().toString(36);
-    
+
 
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    const taskCard = $("<div>")
+    const taskCard = $('<div>')
+        .addClass('card project-card draggable my-3')
+        .attr('data-project-id', project.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(project.name);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(project.type);
+    const cardDueDate = $('<p>').addClass('card-text').text(project.dueDate);
+    const cardDeleteBtn = $('<button>')
+        .addClass('btn btn-danger delete')
+        .text('Delete')
+        .attr('data-project-id', project.id);
+    cardDeleteBtn.on('click', handleDeleteProject);
 
+    if (project.dueDate && project.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(project.dueDate, 'DD/MM/YYYY');
+
+
+    }
 }
 
-// Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-    $(".draggable").draggable ({
-        opacity:1,
-        zIndex: 100,
-        helper:function (e) {
-            const original = $(e.target).hasClass("ui-draggable")
-            ? $(e.target)
-            : $(e.target).closest("ui-draggable");
-            return original.clone().css({
-                width:original.outerWidth(),
-            });
-        },
-    });
-}
+    // Todo: create a function to render the task list and make cards draggable
+    function renderTaskList() {
+        $(".draggable").draggable({
+            opacity: 1,
+            zIndex: 100,
+            helper: function (e) {
+                const original = $(e.target).hasClass("ui-draggable")
+                    ? $(e.target)
+                    : $(e.target).closest("ui-draggable");
+                return original.clone().css({
+                    width: original.outerWidth(),
+                });
+            },
+        });
+    }
 
-// Todo: create a function to handle adding a new task
-function handleAddTask(event) {
+    // Todo: create a function to handle adding a new task
+    function handleAddTask(event) {
         event.preventDefault()
         const load = {
             id: generateTaskId(),
-            title: $("decree").val(),
-            description: $("duration").val(),
-            dueDate: $("directive").val(),
+            title: $("ml-decree").val(),
+            description: $("ml-duration").val(),
+            dueDate: $("ml-directive").val(),
             status: "to-do",
         }
-    localStorage.setItem("load", JSON.stringify(task));
-    location.assign("index.html");
-    console.log("Salutation")
-}
+        localStorage.setItem("load", JSON.stringify(task));
+        location.assign("index.html");
+        console.log("Salutation")
+    }
 
-// Todo: create a function to handle deleting a task
-function handleDeleteTask(event) {
+    // Todo: create a function to handle deleting a task
+    function handleDeleteTask(event) {
+        const cardDeleteBtn = $('<button>')
+            .addClass('btn btn-danger delete')
+            .text('Delete')
+            .attr('data-project-id', project.id);
+        cardDeleteBtn.on('click', handleDeleteProject);
 
-}
+    }
 
-// Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
+    // Todo: create a function to handle dropping a task into a new status lane
+    function handleDrop(event, ui) {
 
-}
+    }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
-// Made sure to add date picker with query
-            $("submit").on("click", handleAddTask);
-            $("#due-date").datepicker({
-                changeMonth: true,
-                changeYear: true,
-            });
-});
+    // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+    $(document).ready(function () {
+        // Made sure to add date picker with query
+        $("submit").on("click", handleAddTask);
+        $("#due-date").datepicker({
+            changeMonth: true,
+            changeYear: true,
+        });
+    });
